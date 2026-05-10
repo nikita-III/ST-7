5e5e89330997d5e1eeb17f7d08a5700f75f2a2f9
nikita-iii <timohaflekss@gmail.com>
2026-05-10 12:54:47 +0300

    st-7-5

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -28,7 +28,7 @@ public class App {
         }
         // 2
         Task2.MainAct();
-        // 3
+        // 3  5/10 %/min
         Task3.MainAct();
         return;
     }
--- src/main/java/com/mycompany/app/Task3.java
+++ src/main/java/com/mycompany/app/Task3.java
@@ -19,7 +19,7 @@ public class Task3 {
             //System.out.println(elem.substring(elem.lastIndexOf("time") + "time: ".length(), elem.lastIndexOf("temperature_2m") - 2));
             int ind = elem.lastIndexOf("time") + "time".length() + 3;
             while (ind < elem.length() && !elem.substring(ind, ind + "_2026-05-07T00:00_".length()).contains("temperature_2m")) {
-                System.out.println(elem.substring(ind + 1, ind + "_2026-05-07T00:00_".length() - 1));
+                //System.out.println(elem.substring(ind + 1, ind + "_2026-05-07T00:00_".length() - 1));
                 sts.add(elem.substring(ind + 1, ind + "_2026-05-07T00:00_".length() - 1).replace('T', ' '));
                 ind += "_2026-05-07T00:00__".length();
             }
@@ -27,34 +27,51 @@ public class Task3 {
             ind = elem.lastIndexOf("temperature_2m") + "temperature_2m".length() + 3;
             ArrayList<String> shs = new ArrayList<String>();
             int indd = 0;
+            int f = 0;
             String ds = "";
-            while (ind < elem.length() && !elem.substring(ind, ind + indd).contains("rain")) {
+            while (ind < elem.length() && !elem.substring(ind, ind + indd).contains("rain") && f == 0) {
                 indd = elem.substring(ind).indexOf(",");
-                ds = elem.substring(ind, ind + indd);
-                if(ds.contains("]")) {
-                    ds = ds.substring(0, ds.length() - 1);
+                //System.out.println(-3);
+
+                //System.out.println(indd);
+                //System.out.println(":)");
+                if (ind < elem.length()) {
+                    ds = elem.substring(ind, ind + indd);
+                    //System.out.println(-2);
+                    if(ds.contains("]")) {
+                        ds = ds.substring(0, ds.length() - 1);
+                        f = 1;
+                    //System.out.println(-1);
+                    }
+                    //System.out.println(ds);
+                    shs.add(ds);
                 }
-                System.out.println(ds);
-                shs.add(ds);
                 ind += indd + 1;
             }
             //System.out.println(elem.substring(elem.lastIndexOf("rain") + "rain: ".length(), elem.lastIndexOf("]") + 1));
             ArrayList<String> srs = new ArrayList<String>();
+                //System.out.println(0);
             ind = elem.lastIndexOf("rain") + "rain".length() + 3;
             while (ind < elem.length() && !ds.contains("]")) {
                 indd = elem.substring(ind).indexOf(",");
+                //System.out.println(1);
                 if (indd < 0) {
                     indd = elem.substring(ind).indexOf("}");
+                //System.out.println(2);
                 }
                 ds = elem.substring(ind, ind + indd);
+                //System.out.println(3);
                 if(ds.contains("]")) {
-                    System.out.println(ds.substring(0, ds.length() - 1));
+                    //System.out.println(ds.substring(0, ds.length() - 1));
                     srs.add(ds.substring(0, ds.length() - 1));
+                //System.out.println(4);
                 } else {
-                    System.out.println(ds);
+                    //System.out.println(ds);
                     srs.add(ds);
+                //System.out.println(5);
                 }
                 ind += indd + 1;
+                //System.out.println(6);
             }
             // time temperature_2m rain _2026-05-07T00:00_,
             try (BufferedWriter writer = new BufferedWriter(new FileWriter("result/forecast.txt"))) {
